e64f2bd163fefcb26563e37c06f6d05476979ce6
quynh2204 <phuongquynh0204cm@gmail.com>
2024-10-14 00:44:47 +0700

    Update GiaoDien.cs

--- GiaoDien.cs
+++ GiaoDien.cs
@@ -26,17 +26,17 @@ namespace UEH_Green
             @"║                 @@@@@@@@@    @@@     @@@   @@@   @@@         @@@   @@@@@         @@@@@@@@@@@   @@@                   ║",
             @"║                  @@@@@@@     @@@     @@@   @@@   @@@@@@@@@   @@@    @@@@          @@@@@@@@@    @@@                   ║",
             @"║                                                                                                                      ║",
-            @"║             @@@     @@@     @@@@@@@     @@@@    @@@   @@@     @@@                         ccc@@\                     ║",
-            @"║             @@@     @@@    @@@@@@@@@    @@@@@   @@@   @@@     @@@               c@@@@@@\ /ccc@@@|                    ║",
-            @"║              @@@   @@@    @@@     @@@   @@@@@@  @@@   @@@     @@@              /ccc@@@@@\@ccc@@/ccc\                 ║",
-            @"║                @@@@@      @@@@@@@@@@@   @@@ @@@ @@@   @@@@@@@@@@@              cccccccc@@\@@@/cccc@@                 ║",
-            @"║              @@@   @@@    @@@     @@@   @@@  @@@@@@   @@@     @@@               \ccccccc@|cccccc@@@/                 ║",
-            @"║             @@@     @@@   @@@     @@@   @@@   @@@@@   @@@     @@@                  \ccccc@ccc@@@/                    ║",
-            @"║             @@@     @@@   @@@     @@@   @@@    @@@@   @@@     @@@                  |           |                     ║",
+            @"║            @@@     @@@     @@@@@@@     @@@@    @@@   @@@     @@@                          ccc@@\                     ║",
+            @"║            @@@     @@@    @@@@@@@@@    @@@@@   @@@   @@@     @@@                c@@@@@@\ /ccc@@@|                    ║",
+            @"║             @@@   @@@    @@@     @@@   @@@@@@  @@@   @@@     @@@               /ccc@@@@@\@ccc@@/ccc\                 ║",
+            @"║               @@@@@      @@@@@@@@@@@   @@@ @@@ @@@   @@@@@@@@@@@               cccccccc@@\@@@/cccc@@                 ║",
+            @"║             @@@   @@@    @@@     @@@   @@@  @@@@@@   @@@     @@@                \ccccccc@|cccccc@@@/                 ║",
+            @"║            @@@     @@@   @@@     @@@   @@@   @@@@@   @@@     @@@                   \ccccc@ccc@@@/                    ║",
+            @"║            @@@     @@@   @@@     @@@   @@@    @@@@   @@@     @@@                   |           |                     ║",
             @"║                                                                                   /*  >     <  *\                    ║",
-            @"║                                               ╔════════════════════════╗         |****   0   ****|                   ║",
-            @"║                                               ║      >>> Enter <<<     ║         |***************|                   ║",
-            @"║                                               ╚════════════════════════╝                                             ║",
+            @"║                                             ╔════════════════════════╗           |****   0   ****|                   ║",
+            @"║                                             ║      >>> Enter <<<     ║           |***************|                   ║",
+            @"║                                             ╚════════════════════════╝                                               ║",
             @"║                                                                                                                      ║",
             @"║                                                                                                                      ║",
             @"╚══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╝"
@@ -48,6 +48,33 @@ namespace UEH_Green
             }
             Console.ResetColor();
         }
+        public static void PrintEnterNFBanner(string[] banner)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            int consoleWidth = Console.WindowWidth;
+            int consoleHeight = Console.WindowHeight;
 
+            // Tính toán vị trí để in banner ra giữa màn hình
+            int xPos = (consoleWidth - banner[0].Length) / 4;
+            int yPos = (consoleHeight - banner.Length) / 4;
+
+            // In banner ở vị trí đã tính toán
+            for (int i = 0; i < banner.Length; i++)
+            {
+                Console.SetCursorPosition(Console.WindowWidth/7, yPos + i);
+                Console.WriteLine(banner[i]);
+                if (banner[i].Contains("╔" + "═" + "╚" + "║" + "╝")) // Đặt màu xanh lá cho phần có chữ 'c'
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+
+                }
+
+                else // Đặt màu trắng cho phần còn lại
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                }
+            }
+            Console.ResetColor();
+        }
     }
 }
