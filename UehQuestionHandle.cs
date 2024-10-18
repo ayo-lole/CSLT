0c878b2e0ed8980d9d10ea587320459c8c95fadc
quynh2204 <phuongquynh0204cm@gmail.com>
2024-10-18 17:30:16 +0700

    Update UehQuestionHandle.cs

--- UehQuestionHandle.cs
+++ UehQuestionHandle.cs
@@ -11,15 +11,15 @@ namespace UEH_Green
         public static void PositionAtBottom()
         {
             int windowHeight = Console.WindowHeight;
-            int bottomPosition = windowHeight = 18;
+            int bottomPosition = windowHeight = 17; //Đặt vị trí câu hỏi trên màn hình console. Giới hạn khung hình hiện câu hỏi 
 
             Console.SetCursorPosition(0, bottomPosition);
         }
         public static void ClearQuestionAndAnswer(int startLine, int lineCount)
         {
-            for (int i = 0; i < lineCount; i++)
+            for (int i = 0; i < lineCount + 2 ; i++) //Đếm số dòng sẽ xóa
             {
-                Console.SetCursorPosition(0, startLine + i);
+                Console.SetCursorPosition(0, startLine + i); //Đặt con trỏ chuột tại từ vị trí in câu hỏi ra đến xuống dòng trả lời
                 Console.Write(new string(' ', Console.WindowWidth));
             }
             Console.SetCursorPosition(0, startLine);
